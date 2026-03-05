46d7d18a933a1264013b89636fcc10e9519bef1c
GitHub Action <action@github.com>
2026-03-05 04:50:04 +0000

    auto generate pages [skip ci]

--- components/DocumentList.tsx
+++
@@ -1,9 +0,0 @@
-export default function DocumentList({ items }: { items: string[] }) {
-  return (
-    <ul className="list-disc space-y-2 pl-5 text-gray-800">
-      {items.map((d, idx) => (
-        <li key={idx}>{d}</li>
-      ))}
-    </ul>
-  );
-}
